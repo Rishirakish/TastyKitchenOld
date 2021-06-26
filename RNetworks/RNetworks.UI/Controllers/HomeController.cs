500c050db89d85e3617cad03e6b812e5d90473e3
Rishirakish <rishiroser@gmail.com>
2021-06-26 19:04:49 +0530

    Tk old

---
+++ RNetworks/RNetworks.UI/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using OfficeOpenXml;
+
+namespace RNetworks.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //using OfficeOpenXml;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(@"C:\RK\Personal\FoodBusiness\kitchen materials\TastyKitchenExpenses.xlsx")))
+            {
+                var myWorksheet = xlPackage.Workbook.Worksheets.First(); //select sheet here
+                var totalRows = myWorksheet.Dimension.End.Row;
+                var totalColumns = myWorksheet.Dimension.End.Column;
+
+                var sb = new StringBuilder(); //this is your data
+                for (int rowNum = 2; rowNum <= totalRows; rowNum++) //select starting row here
+                {
+                    var row = myWorksheet.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                    sb.AppendLine(string.Join(",", row));
+                }
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
